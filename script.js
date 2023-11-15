b5e0ef128cfa35aa1c39f4991878ae040f1c27dc
peihsin <peihsin0715@gamil.com>
2023-11-15 21:12:26 +0800

    Frist version

---
+++ script.js
@@ -0,0 +1,26 @@
+var modal = document.getElementById("videoModal");
+
+var span = document.getElementsByClassName("close")[0];
+
+function showPopup(videoSrc, description) {
+    var modal = document.getElementById('videoModal');
+    var modalVideo = document.getElementById('modalVideo');
+    var modalText = document.getElementById('modalText');
+
+    modalVideo.src = videoSrc;
+    modalText.innerHTML = description;
+
+    modal.style.display = 'block';
+
+    var span = document.getElementsByClassName('close')[0];
+    span.onclick = function() {
+        modal.style.display = 'none';
+    }
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById('videoModal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
