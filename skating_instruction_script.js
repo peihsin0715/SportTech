735077e98cd5fa3c88bed27b1b3dfff229b2c0f0
peihsin <peihsin0715@gamil.com>
2023-11-21 22:19:11 +0800

    Redirect fixed

---
+++ skating_instruction_script.js
@@ -0,0 +1,34 @@
+var modal = document.getElementById("videoModal");
+
+var span = document.getElementsByClassName("close")[0];
+
+function showPopup(videoSrc, description) {
+    var modal = document.getElementById('videoModal');
+    var modalVideo = document.getElementById('modalVideo');
+    var modalText = document.getElementById('modalText');
+
+    modalVideo.src = videoSrc;
+    modalText.innerHTML = description;
+
+    modal.style.display = 'block';
+
+    var span = document.getElementsByClassName('close')[0];
+    span.onclick = function() {
+        modal.style.display = 'none';
+    }
+}
+
+function ClickNext(){
+    window.location.replace("./skating_align.html");
+}
+
+function ToSkating(){
+    window.location.replace("./skating_instruction.html");
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById('videoModal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
