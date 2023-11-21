5f217a5b655cc42eeb0fdca3fb038091c5b4872f
b10815061 <c1l1mo@gmail.com>
2023-11-21 20:45:40 +0800

    add: add boxing alignment page

--- align_script.js
+++ align_script.js
@@ -4,22 +4,32 @@ var span = document.getElementsByClassName("close")[0];
 
 function showPopup(videoSrc, description) {
     var modal = document.getElementById('videoModal');
-    var modalVideo = document.getElementById('modalVideo');
-    var modalText = document.getElementById('modalText');
+    var modalVideo = document.getElementById('AlignmentVideo');
 
     modalVideo.src = videoSrc;
-    modalText.innerHTML = description;
 
     modal.style.display = 'block';
 
     var span = document.getElementsByClassName('close')[0];
     span.onclick = function() {
         modal.style.display = 'none';
+        document.removeEventListener('keydown', handleEscape);
     }
+
+    // Event listener for Escape key
+    function handleEscape(event) {
+        if (event.key === 'Escape') {
+            modal.style.display = 'none';
+            document.removeEventListener('keydown', handleEscape);
+        }
+    }
+
+    // Add event listener when modal is opened
+    document.addEventListener('keydown', handleEscape);
 }
 
-function ClickNext(){
-    window.location.replace("./align.html");
+function ClickPrevious(){
+    window.location.replace("./index.html");
 }
 
 window.onclick = function(event) {
